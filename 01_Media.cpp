604f75caa5454fdb645ac6c6223f519221f7c7b4
Ericlys Lima <ericlyslima05@gmail.com>
2020-12-20 19:31:04 -0300

    Add_Media
    Exercicio Media

---
+++ 01_Media.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+	
+	float n1,n2,media;
+	
+	cout << "DIGITE A PRIMEIRA NOTA: ";
+	cin >> n1;
+	cout << "DIGITE A SEGUNDA NOTA: ";
+	cin >> n2;
+	
+	media=(n1+n2)/2;
+	
+	system("cls");
+	
+	cout << "SUA MEDIA E: "<< media;
+
+	return 0;
+
+}
+
